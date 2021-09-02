14aa3b14da21e8a4b95aade6f54d2dd1733d839d
Fatma Azra Yildiz <fatma1822yildiz@hotmail.com>
2021-09-02 14:47:11 +0200

    hello

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-const Person = require("./person");
-const Photo = require("./photo");
+const Person = require('./person')
+const Photo = require('./photo')
 
-const azra = new Person("azra", 43);
-const karen = new Person("karen", 48);
-const bedo = new Person("bedo", 17);
-const jonathan = new Person("jonathan", 75);
+const azra = new Person('azra', 43)
+const karen = new Person('karen', 48)
+const bedo = new Person('bedo', 17)
+const jonathan = new Person('jonathan', 75)
 
+const berlinPhoto = new Photo('berlin.jpg')
+const munichPhoto = new Photo('koeln.jpg')
 
-const berlinPhoto = new Photo("berlin.jpg");
-const munichPhoto = new Photo("koeln.jpg");
+jonathan.addPhoto(berlinPhoto)
+jonathan.addPhoto(munichPhoto)
 
-jonathan.addPhoto(berlinPhoto); 
-jonathan.addPhoto(munichPhoto);
+jonathan.bio = 'An awesome guy who has seen it all, and now sharing them all with you.'
 
-jonathan.bio = 
-"An awesome guy who has seen it all, and now sharing them all with you.";
+karen.likePhoto(berlinPhoto)
+jonathan.addPhoto(berlinPhoto)
+azra.likePhoto(berlinPhoto)
+bedo.addPhoto(berlinPhoto)
 
-karen.likePhoto(berlinPhoto);
-jonathan.addPhoto(berlinPhoto);
-azra.likePhoto(berlinPhoto);
-bedo.addPhoto(berlinPhoto);
+// console.log(jonathan.profile);
 
-
-//console.log(jonathan.profile); 
-
-
-
-console.log(jonathan.profile); 
-
+console.log(jonathan.profile)
