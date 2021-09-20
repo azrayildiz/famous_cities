f76c7a0ac744baada13f5c0d57b07fedbd9af792
Fatma Azra Yildiz <fatma1822yildiz@hotmail.com>
2021-09-20 21:30:23 +0200

    Added talkEventSchema

--- src/models/talk-event.js
+++ src/models/talk-event.js
@@ -1,12 +1,56 @@
+// const year = new Date()
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
+
+const talkEventSchema = new mongoose.Schema({
+  tourName: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  eventDate: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  likedBy: [],
+  comments: [],
+  attends: [],
+  saved: [],
+  payments: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  cancelledBy: [
+    {
+      type: String,
+    },
+  ],
+})
 class TalkEvent {
-  constructor(filename) {
-    this.filename = filename
-    this.likedBy = []
-    this.comments = []
-    this.attended = []
-    this.payments = []
-    this.cancelledBy = []
+  async addPhoto(photo) {
+    this.photo.push(photo)
+    await this.save()
+  }
+  async addInfo(info) {
+    this.info = info
+    await this.save()
+  }
+  async addDate(date) {
+    this.date.push(date)
+
+    await this.save()
   }
 }
 
-module.exports = TalkEvent
+talkEventSchema.loadClass(TalkEvent)
+talkEventSchema.plugin(autopopulate)
+
+module.exports = mongoose.model('TalkEvent', talkEventSchema)
