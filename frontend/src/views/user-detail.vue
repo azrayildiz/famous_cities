f097b29501d4a2c335ac32b7249a8db14b772dfb
Fatma Azra Yildiz <fatma1822yildiz@hotmail.com>
2021-10-12 10:46:29 +0200

    Import counter

--- frontend/src/views/user-detail.vue
+++ frontend/src/views/user-detail.vue
@@ -1,19 +1,18 @@
 <script>
-import axios from 'axios'
-import Counter from '@components/counter.vue'
+import Counter from '@/components/counter.vue'
 import UserCard from '@/components/user-card.vue'
 import { mapActions } from 'vuex'
 
 export default {
   name: 'UserDetail',
-  components: { USerCard },
+  components: { UserCard, Counter },
   data() {
     return {
       user: null,
     }
   },
   async created() {
-    this.user = await this.fetchUser(this.route.params.id)
+    this.user = await this.fetchUser(this.$route.params.id)
   },
   methods: {
     ...mapActions(['fetchUser']),
@@ -25,4 +24,5 @@ export default {
   .about
     h1 This is a user detail
     UserCard(:user="user" v-if="user")
+    Counter
 </template>
