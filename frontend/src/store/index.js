6cb851961c9b9fb27584c1b4c6c5045e2fd8ba4e
Fatma Azra Yildiz <fatma1822yildiz@hotmail.com>
2021-10-12 10:28:47 +0200

    Add mutations and actions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+
+axios.defaults.baseURL = process.env.VUE_APP_BASE_URL
+axios.defaults.withCredentials = true
 
 Vue.use(Vuex)
 
+const mutations = {
+  INCREMENT_COUNT: 'increment count',
   SET_USER: 'set user',
+}
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
     user: null,
+  },
+  mutations: {
+    [mutations.INCREMENT_COUNT](state) {
+      state.count++
+    },
     [mutations.SET_USER](state, user) {
       state.user = user
+    },
+  },
+  actions: {
+    incrementCount({ commit }) {
+      commit(mutations.INCREMENT_COUNT)
+    },
+    async fetchUser(store, id) {
+      const userRequest = await axios.get(`/api/users/${id}`)
+      return userRequest.data
+    },
+    async fetchUsers() {
+      const usersRequest = await axios.get('/api/users')
+      return usersRequest.data
     },
     async fetchSession({ commit }) {
       const user = await axios.get('/api/account/session')
@@ -30,3 +59,8 @@ Vue.use(Vuex)
   },
   modules: {},
 })
+
+export default async function init() {
+  await store.dispatch('fetchSession')
+  return store
+}
